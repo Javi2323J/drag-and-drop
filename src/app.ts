53827c305e92d1b9f4a6663de39bf899b35cfab5
DESKTOP-C6NF0QP\jrome <jromera232003@gmail.com>
2025-01-02 20:15:37 +0100

    render form #1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 // SINGLE PAGE APPLICATION
+
+// First step ==> make the html templates visible
+
+class ProjectInput {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLFormElement
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input') as HTMLTemplateElement
+        this.hostElement = document.getElementById('app') as HTMLDivElement
+
+        // Automatically render the form
+        const importedNode = document.importNode(this.templateElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLFormElement
+        this.attach()
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const prjInput = new ProjectInput()
